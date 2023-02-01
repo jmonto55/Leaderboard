435d9d5e33213db1d99a483676cf19c021487cbb
jmonto55 <jmonto55@gmail.com>
2023-02-01 13:53:10 -0500

    Add post method for API data

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,17 +1,23 @@
-// const id = 'T7V7ogLg1WciER8lhmRb';
+// Game id = T7V7ogLg1WciER8lhmRb
 const apiURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/T7V7ogLg1WciER8lhmRb/scores';
-const apiHeaders = {
-  headers: {
-    'content-type': 'application/json',
-  },
-};
 
 const getScores = async () => {
   const res = await fetch(apiURL);
   const data = await res.json();
   const stat = res.status;
-  if (stat !== 200) return 'Error';
   return { data, stat };
 };
 
-export default getScores;
+const postScore = async (user, score) => {
+  const res = await fetch(apiURL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ user, score }),
+  });
+  const data = await res.json();
+  const stat = res.status;
+  return { data, stat };
+};
+export { getScores, postScore };
--- src/modules/tools.js
+++ src/modules/tools.js
@@ -1,20 +1,40 @@
-import getScores from './api.js';
+import { getScores, postScore } from './api.js';
 
 const refreshButton = document.querySelector('.refresh_button');
+const form = document.querySelector('#form');
+const username = document.getElementById('name');
+const score = document.getElementById('score');
 
 const refreshScores = async () => {
   const { data, stat } = await getScores();
-  if (stat !== 200) return null;
-
   const scoresList = document.querySelector('.scores_list');
-  scoresList.innerHTML = '';
-  data.result.forEach((e) => {
-    const score = document.createElement('li');
-    score.classList.add('score');
-    score.innerHTML = `${e.user}: ${e.score}`;
-    scoresList.appendChild(score);
-  });
+  if (stat !== 200) {
+    scoresList.innerHTML = `Error ${stat}: ${data.message}`;
+  } else {
+    scoresList.innerHTML = '';
+    data.result.forEach((e) => {
+      const score = document.createElement('li');
+      score.classList.add('score');
+      score.innerHTML = `${e.user}: ${e.score}`;
+      scoresList.appendChild(score);
+    });
+  }
   return scoresList;
 };
 
 refreshButton.onclick = () => refreshScores();
+
+const addToDOM = (user, score) => {
+  const scoreLi = document.createElement('li');
+  scoreLi.innerHTML = `${user}: ${score}`;
+  scoreLi.classList.add('score');
+  document.querySelector('.scores_list').appendChild(scoreLi);
+};
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const scoreUsername = username.value;
+  const scoreValue = score.value;
+  addToDOM(scoreUsername, scoreValue);
+  form.reset();
+};
