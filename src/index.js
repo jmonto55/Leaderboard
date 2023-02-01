d1d3c764c087fd18dedc6ba23b17979c03547d51
jmonto55 <jmonto55@gmail.com>
2023-02-01 13:19:42 -0500

    Add function to show API data in the DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './style.css';
+import './modules/tools.js';
--- src/modules/tools.js
+++ src/modules/tools.js
@@ -1,4 +1,20 @@
-import getScores from "./api";
+import getScores from './api.js';
 
 const refreshButton = document.querySelector('.refresh_button');
+
+const refreshScores = async () => {
+  const { data, stat } = await getScores();
+  if (stat !== 200) return null;
+
+  const scoresList = document.querySelector('.scores_list');
+  scoresList.innerHTML = '';
+  data.result.forEach((e) => {
+    const score = document.createElement('li');
+    score.classList.add('score');
+    score.innerHTML = `${e.user}: ${e.score}`;
+    scoresList.appendChild(score);
+  });
+  return scoresList;
+};
+
 refreshButton.onclick = () => refreshScores();
